23863b88a4af95665be0ecf5156d586d49cd0c8f
johnvimalasir001 <johnvimalasir@gmail.com>
2024-02-17 15:11:14 +0530

    pause completed

--- flappy_bird.py
+++ flappy_bird.py
@@ -3,28 +3,24 @@ import numpy as np
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 300
-bird_img_path = "image/bird.png"
+bird_img_path = "image/bird2.jpg"
 bird_img = cv2.imread(bird_img_path)
-pipe_path = "image/pipe.png"
-pipe_img = cv2.imread(pipe_path)
+# pipe_path = "image/pipe.png"
+# pipe_img = cv2.imread(pipe_path)
 bg_path = "image/bg.jpg"
 bg_img = cv2.imread(bg_path)
 bg = cv2.resize(bg_img, (WINDOW_WIDTH, WINDOW_HEIGHT))
-
 WINDOW_NAME = "Jumping Game"
 OBSTACLE_WIDTH = 30
 OBSTACLE_HEIGHT = 80
-OBSTACLE_COLOR = (0, 0, 0)
+OBSTACLE_COLOR = (0, 200, 0)
 JUMP_HEIGHT = 5
-
 bird_width = 40
 bird_height = 30
 pipe_width = 40
 pipe_height = 80
-
 bird_x = 50
 bird_y = 150
-
 velocity_y = 0
 gravity = 1
 is_jumping = False
@@ -33,6 +29,7 @@ obstacle_y = WINDOW_HEIGHT - OBSTACLE_HEIGHT
 obstacle2_y = WINDOW_HEIGHT - 300
 obstacle_speed = 5
 score = 0
+paused= False
 
 
 def draw_bird(window):
@@ -50,7 +47,6 @@ def draw_bird(window):
 
 def draw_obstacle(window):
     global pipe_img, obstacle_x, obstacle_y
-    obstacle_resize = cv2.resize(pipe_img, (pipe_width, pipe_height))
     cv2.rectangle(window, (obstacle_x, obstacle_y), (obstacle_x + OBSTACLE_WIDTH, obstacle_y + OBSTACLE_HEIGHT),
                   OBSTACLE_COLOR, -1)
 
@@ -70,8 +66,9 @@ def is_colliding():
     return False
 
 
+
 def main():
-    global bird_x, bird_y, velocity_y, is_jumping, obstacle_x, obstacle_speed, score
+    global bird_x, bird_y, velocity_y, is_jumping, obstacle_x, obstacle_speed, score,paused
     cv2.namedWindow(WINDOW_NAME)
     while True:
         frame = bg.copy()
@@ -82,31 +79,40 @@ def main():
         cv2.imshow(WINDOW_NAME, frame)
         key = cv2.waitKey(20)
 
+        if key == ord('p'):
+            paused = True
+
         if key == ord(' '):
-            is_jumping = True
+            paused  = False
+            is_jumping= True
             velocity_y = -10
+
+        
+            
 
         if key == ord('q'):
             break
 
-        if is_jumping:
-            bird_y += velocity_y
-            velocity_y += gravity
+        if paused == False:
 
-            if bird_y >= WINDOW_HEIGHT - bird_height:
-                is_jumping = False
+            if is_jumping:
+                bird_y += velocity_y
+                velocity_y += gravity
 
-        obstacle_x -= obstacle_speed
-        if obstacle_x < 0:
-            obstacle_x = WINDOW_WIDTH
-            score += 1
+                if bird_y >= WINDOW_HEIGHT - bird_height:
+                    is_jumping = False
 
-        if is_colliding():
-            cv2.putText(frame, "Game Over", (300, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
-            cv2.putText(frame, f"Score:{score}", (400, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            cv2.imshow(WINDOW_NAME, frame)
-            cv2.waitKey()
-            break
+            obstacle_x -= obstacle_speed
+            if obstacle_x < 0:
+                obstacle_x = WINDOW_WIDTH
+                score += 1
+
+            if is_colliding():
+                cv2.putText(frame, "Game Over", (300, 150), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
+                cv2.putText(frame, f"Score:{score}", (400, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                cv2.imshow(WINDOW_NAME, frame)
+                cv2.waitKey()
+                break
 
     cv2.destroyAllWindows()
 
